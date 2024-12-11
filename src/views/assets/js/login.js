01a655bb640ee089716ce0f906776cb0b86f001a
Davi Milioli <davi.milioli@inforce.com.br>
2024-12-11 17:58:46 -0300

    Content load no login.js

--- src/views/assets/js/login.js
+++ src/views/assets/js/login.js
@@ -1,39 +1,41 @@
-function login() {
-    const form = document.querySelector('[data-form]');
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    login();
+    console.log('login.js inicializado');
+    
+    function login() {
+        const form = document.querySelector('[data-form]');
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
 
-        const formData = new FormData(form);
+            const formData = new FormData(form);
 
-        const data = {
-            email: formData.get('email'),
-            password: formData.get('password'),
-        };
+            const data = {
+                email: formData.get('email'),
+                password: formData.get('password'),
+            };
 
-        try {
-            const response = await fetch('/login/auth', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            });
+            try {
+                const response = await fetch('/login/auth', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
 
-            if (!response.ok) {
-                const error = await response.json();
-                console.error('Erro:', error.message);
-                alert(`Erro: ${error.message}`);
-                return;
+                if (!response.ok) {
+                    const error = await response.json();
+                    console.error('Erro:', error.message);
+                    alert(`Erro: ${error.message}`);
+                    return;
+                }
+
+                const result = await response.json();
+                console.log(result);
+
+            } catch (error) {
+                console.error('Erro ao enviar formulário:', error);
             }
-
-            const result = await response.json();
-            console.log(result);
-
-            alert(`Sucesso: ${result.message}`);
-        } catch (error) {
-            console.error('Erro ao enviar formulário:', error);
-        }
-    });
-}
-
-login();
+        });
+    }
+});
