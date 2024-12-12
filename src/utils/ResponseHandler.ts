dddc3ba2b5129949b16cbcc69d4f37c1b414094a
Davi Milioli <davi.milioli@inforce.com.br>
2024-12-12 11:36:42 -0300

    utilidade para controlar as respostas de requisição

---
+++ src/utils/ResponseHandler.ts
@@ -0,0 +1,20 @@
+class ResponseHandler {
+    static success(message: string, statusCode: number, data: any): { status: string, message: string, statusCode: number, data: any } {
+        return {
+            status: 'success',
+            message,
+            statusCode,
+            data,
+        };
+    }
+
+    static error(message: string, statusCode: number): { status: string, message: string, statusCode: number } {
+        return {
+            status: 'error',
+            message,
+            statusCode,
+        };
+    }
+}
+
+export default ResponseHandler;
