31423eaae07a5b2a86d003af9cb30cebef8ad613
Davi Milioli <davi.milioli@inforce.com.br>
2024-12-18 17:00:48 -0300

    feat: página inicial

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import LoginController from "../controllers/LoginController";
+import HomeController from "../controllers/HomeController";
 
 const router = Router();
+
+router.get('/', (req, res) => {
+    const homeController = new HomeController(req, res);
+    homeController.index();
+})
 
 router.get('/login', (req, res) => {
     const loginController = new LoginController(req, res); 
